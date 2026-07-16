c9c7f93625d323db5a82d214cf386525762c482e
mahdzd <110607161+mahdzd@users.noreply.github.com>
2026-07-16 13:43:43 +0300

    Update ToF LastYear.py

--- Hardware/ToF LastYear.py
+++ Hardware/ToF LastYear.py
@@ -57,9 +57,9 @@ class DistanceSensor:
 
 
 dist_sensors=[] 
-dist_sensors_pins=[17,27] #GPIo numbering
-dist_sensors_Addresses=[0x30,0x31]  # I2C addresses
-for i in range(2):
+dist_sensors_pins=[17,27,22,23] #GPIo numbering
+dist_sensors_Addresses=[0x30,0x31,0x32,0x33]  # I2C addresses
+for i in range(4):
     dist_sensors.append(DistanceSensor(dist_sensors_pins[i]))#create a sensor object and intlize it and add it to the array
     dist_sensors[i].change_address(dist_sensors_Addresses[i])#give each sensor a costum address
 
