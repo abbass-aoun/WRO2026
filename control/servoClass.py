3f3ffaec1e9421b44ba45b64ca6ea4c04a83b9b9
abbass-aoun <abbass.aoun@icloud.com>
2026-07-24 12:20:14 +0300

    Update car_controller and servoClass

--- control/servoClass.py
+++ control/servoClass.py
@@ -32,7 +32,7 @@ class myServo:
     """
 
     def __init__(self, servo_pin: int,
-                 center_angle: int = 50,
+                 center_angle: int = 80,
                  max_deviation: int = 27):
         """
         Args:
